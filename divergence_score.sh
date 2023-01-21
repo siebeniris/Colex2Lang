e88eba12b40dfdf4ff735b95e107a877d92854bc
Yiyi Chen <chen.yiyi@protonmail.com>
2023-01-21 22:07:19 +0100

    script

--- divergence_score.sh
+++ divergence_score.sh
@@ -11,4 +11,6 @@ conda activate graphEmb
 
 cd $HOME/ColexGraph/src/graphs/Comparing_Graph_Embeddings
 
-python get_divergence_scores.py divergence.csv ~/ColexGraph/data/eval_nodeEmb/
+#python get_divergence_scores.py divergence.csv ~/ColexGraph/data/eval_nodeEmb/
+
+./GED -g /home/cs.aau.dk/ng78zb/ColexGraph/data/eval_nodeEmb/wn_concept/edgelists_wn_concept -c /home/cs.aau.dk/ng78zb/ColexGraph/data/eval_nodeEmb/wn_concept/wn_concept.ecg -e /home/cs.aau.dk/ng78zb/ColexGraph/data/eval_nodeEmb/wn_concept/prone_embeddings
