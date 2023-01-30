3c9017c5cf7e184db95470b309f2021009ba0df8
Yiyi Chen <chen.yiyi@protonmail.com>
2023-01-30 13:42:47 +0100

    sbatch script

---
+++ run_one_ff_classifier_prone.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#SBATCH --partition=prioritized
+#SBATCH --job-name=oneff
+#SBATCH --output=%j.out
+#SBATCH --time=8:00:00
+#SBATCH --mem=32G
+
+source $HOME/.bashrc
+conda activate graphEmb
+
+cd $HOME/ColexGraph
+
+
+
+env=$1
+device=$2
+
+
+conda activate "$env"
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics add+avg
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics add+max
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics add+sum
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics concat+avg
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics concat+max
+
+python -m src.feature_prediction.run "$device" output/models oneff 100 prone clics concat+sum
