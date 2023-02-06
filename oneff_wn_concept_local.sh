4d5bcfa9578dba026ee9160034a48f41d32dedf6
Yiyi Chen <chen.yiyi@protonmail.com>
2023-02-06 10:21:57 +0100

    bash script

--- oneff_wn_concept_local.sh
+++ oneff_wn_concept_local.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
 
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn
 
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc concat+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc concat+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc concat+max
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc add+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc add+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn ggvc add+max
-
-
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove concat+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove concat+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove concat+max
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove add+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove add+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn glove add+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc concat+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc concat+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc concat+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc add+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc add+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept ggvc add+max
 
 
-
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec concat+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec concat+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec concat+max
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec add+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec add+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn node2vec add+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove concat+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove concat+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove concat+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove add+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove add+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept glove add+max
 
 
 
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec concat+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec concat+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec concat+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec add+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec add+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept node2vec add+max
 
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone concat+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone concat+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone concat+max
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone add+sum
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone add+avg
-python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn prone add+max
+
+
+
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone concat+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone concat+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone concat+max
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone add+sum
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone add+avg
+python -m src.feature_prediction.run_kfold cpu output/ oneff 100 wn wn_concept prone add+max
 
